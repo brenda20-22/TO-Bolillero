87b8c2fa58ab50e6214ebc7fc8591986aa1783a9
brenda20-22 <duranpbrenda@gmail.com>
2023-05-05 13:35:21 -0300

    ultimo test

--- TestPosta/TestBolillero.cs
+++ TestPosta/TestBolillero.cs
@@ -5,22 +5,50 @@ namespace Test;
 public class TestBolillero
 {
     public Bolillero bolillero { get; set; }
-    public TestBolillero() => bolillero = new Bolillero(10 , new EPrimera());
+    public TestBolillero() => bolillero = new Bolillero(10, new EPrimera());
 
     [Fact]
     public void SacarBolilla()
     {
         var bolilla = bolillero.SacarBolilla();
 
-        Assert.Equal(0,bolilla);
+        Assert.Equal(0, bolilla);
         Assert.Equal(9, bolillero.Adentro.Count);
         Assert.Equal(1, bolillero.Afuera.Count);
     }
 
     [Fact]
-    public void TestName()
+    public void ReingresarBolilla()
     {
+        var numero = bolillero.SacarBolilla();
+        var espera = 0;
+        var espera1 = 10;
 
+        Assert.Equal(numero, espera);
+        Assert.Equal(espera, bolillero.Adentro.Count);
+        Assert.Equal(espera1, bolillero.Afuera.Count);
+
+    }
+
+    [Fact]
+    public void JugarGana()
+    {
+        var victoria = bolillero.Jugar(jugada: new List<int> { 0, 1, 2, 3 });
+        Assert.True(victoria);
+    }
+
+    [Fact]
+    public void JugarPierde()
+    {
+        var pierde = bolillero.Jugar(jugada: new List<int> { 4, 2, 3 });
+        Assert.False(pierde);
+    }
+
+    [Fact]
+    public void JugarNveces()
+    {
+        var jugada = bolillero.JugarNveces(jugada: new List<int> { 0, 1 }, cantidad: 1);
+        Assert.Equal(1, jugada);
     }
 
 }
